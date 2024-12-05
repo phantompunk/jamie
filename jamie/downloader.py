30898a13cd95127fe67c6c83ddf3319f315c5272
Rodrigo Moran <rigo.moran0@gmail.com>
2024-12-05 19:21:49 +0000

    fix: splitter using segments

--- jamie/downloader.py
+++ jamie/downloader.py
@@ -5,7 +5,7 @@ import yt_dlp
 def download_audio(urls: list[str], filename: str, output_dir: str = "./audio"):
     if not output_dir:
         print("No output")
-    # cookies = browser_cookie3.brave(domain_name='youtube.com')
+
     ydl_opts = {
         "format": "mp3/bestaudio/best",
         "outtmpl": os.path.join(output_dir, f"{filename}.%(ext)s"),
@@ -16,15 +16,8 @@ def download_audio(urls: list[str], filename: str, output_dir: str = "./audio"):
             }
         ],
     }
-    # with open('cookies.txt', 'w') as f:
-    #     for cookie in cookies:
-    #         f.write(f'{cookie.domain}\tTRUE\t{cookie.path}\t{cookie.secure}\t'
-    #                 f'{cookie.expires}\t{cookie.name}\t{cookie.value}\n')
-    #
-    # Add cookie file to yt-dlp options
+
     ydl_opts["cookiefile"] = "cookies.txt"
-    # if cookiefile:
-    #     ydl_opts.update(cookiefile=cookiefile)
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         _ = ydl.download(urls)
--- jamie/splitter.py
+++ jamie/splitter.py
@@ -9,26 +9,28 @@ DURATION_5_MIN = "300"
 
 def split_audio(
     filename,
-    output_dir: str = "./audio",
+    output_dir: str = "./audio/splits",
     duration: str = DURATION_5_MIN,
     extension: str = ".mp3",
 ):
     if not duration:
         duration = DURATION_5_MIN
 
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
     basename = Path(os.path.basename(filename)).stem
-    # filename += ".mp3"
     command = [
         "ffmpeg",
         "-i",
-        os.path.join(output_dir, filename + extension),
+        os.path.join("./audio", filename + extension),
         "-f",
         "segment",
         "-segment_time",
         duration,
         "-c",
         "copy",
-        os.path.join("./splits/", basename) + "-%03d.mp3",
+        os.path.join(output_dir, basename) + "-%03d.mp3",
     ]
 
     subprocess.run(command, check=True)
