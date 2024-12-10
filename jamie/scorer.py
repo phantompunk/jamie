fa86c2291beef1d58d27c99b2c6e255d8437307e
Rodrigo Moran <rigo.moran0@gmail.com>
2024-12-10 09:51:00 +0100

    fix: scoring method

--- jamie/scorer.py
+++ jamie/scorer.py
@@ -13,26 +13,34 @@ def extract_score(res: str):
         return re.sub(r"<(score|\/score)>", "", res)
 
 def extract_quote(res: str):
-    try:
-        return str(json.loads(res))
-    except ValueError:
-        return re.sub(r"<(score|\/score)>", "", json.loads(res))
+    res = res.strip('"')
+    # print(f"Quote {res}")
+    return res
+    # try:
+    #     return str(json.loads(res))
+    # except ValueError as e:
+    #     print(f"Error parsing JSON: {e}")
+    #     return re.sub(r"<(score|\/score)>", "", json.loads(res))
 
 
-def score_quotes(filename: str):
+def score_quotes(filename: str, model: str="quote"):
+    basepath = "./"
     if ".json" not in filename:
         filename += ".json"
+        basepath = "./transcripts/"
 
-    with open(os.path.join("./transcripts", filename), "r+") as file:
+    with open(os.path.join(basepath, filename), "r+", encoding="utf-8") as file:
         quotes = json.load(file)
 
     for quote in tqdm(quotes, desc="Scoring quotes"):
         response = ollama.generate(
-            model="edit", prompt=json.dumps(quote["quote"]), stream=False
+            model=model, prompt=json.dumps(quote["quote"]), stream=False
         )
         score = extract_quote(response.get("response"))
-        quote.update(selected=score)
+        if score != "SKIP":
+            quote.update(selected=score)
 
-    with open(os.path.join("./scored", filename), "w") as output:
+    with open(os.path.join(basepath, filename), "w") as output:
         json.dump(quotes, output, indent=4)
+    # print(json.dumps(quotes, indent=4))
     return quotes
