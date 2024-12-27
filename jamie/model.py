b6049689981d5f4d360525e090a7eb51e4ec5785
Rodrigo Moran <rigo.moran0@gmail.com>
2024-12-27 11:31:00 -0500

    fix: add timestamp to quote model

--- jamie/model.py
+++ jamie/model.py
@@ -3,13 +3,14 @@ import hashlib
 
 
 class Quote:
-    def __init__(self, quote: str, speaker: str, episode: str = "", link: str = ""):
+    def __init__(self, quote: str, speaker: str, episode: str = "", link: str = "", start:int=0):
         self.quote = quote
         self.speaker = speaker
         self.episode = episode
         self.link = link
         self.id = hashlib.md5(quote.encode()).hexdigest()[:6]
         self.created = datetime.now().isoformat()
+        self.start = start
 
     def to_dict(self):
         return {
@@ -18,5 +19,6 @@ class Quote:
             "episode": self.episode,
             "link": self.link,
             "id": self.id,
+            "start": self.start,
             "created": self.created,
         }
