eeb01cee80b4966292ecac4cd3c6db1a5093146f
Rodrigo Moran <rigo.moran0@gmail.com>
2024-12-29 23:06:50 -0500

    fix: support other audio formats

--- jamie/downloader.py
+++ jamie/downloader.py
@@ -2,33 +2,36 @@ import os
 from typing import Optional
 import yt_dlp
 
-EXTENSION_DEFAULT = "mp3"
 EXTENSION_MP3 = "mp3/bestaudio/best"
+EXTENSION_M4A = "m4a/bestaudio/best"
 FILENAME_TEMPLATE = "%(title)s.%(ext)s"
 
 
 def extension(format: str):
-    if format.lower() in [".mp3", "mp3"]:
-        return EXTENSION_MP3
-    return EXTENSION_MP3
+    ext = {
+        "mp3": EXTENSION_MP3,
+        "m4a": EXTENSION_M4A,
+    }
+    return ext.get(format)
+
+
+def yt_dlp_monitor(d):
+    return d.get("info_dict").get("_filename")
 
 
 def download_audio(
     url: str,
     output: Optional[str] = FILENAME_TEMPLATE,
-    format: str = EXTENSION_DEFAULT,
+    format: str = "mp3",
 ):
-    # templ = os.path.join(output_dir, "%(title)s.%(ext)s")
-    # if filename:
-    #     templ = os.path.join(output_dir, f"{filename}.%(ext)s")
-
+    format = format.replace(".", "").lower()
     ydl_opts = {
         "format": extension(format),
         "outtmpl": output,
         "postprocessors": [
-            {
+            {  # Extract audio using ffmpeg
                 "key": "FFmpegExtractAudio",
-                "preferredcodec": "mp3",
+                "preferredcodec": format,
             }
         ],
     }
@@ -36,4 +39,4 @@ def download_audio(
     ydl_opts["cookiefile"] = "cookies.txt"
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        _ = ydl.download(url)
+        ydl.download(url)
